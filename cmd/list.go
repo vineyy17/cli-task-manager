0d59261064b02a2b477bd977d0f8577a24918f29
Divine <officialkb17@gmail.com>
2024-09-09 18:50:15 +0100

    Sync "All Tasks" function to list command

--- cmd/list.go
+++ cmd/list.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/vineyy17/cli-task-manager/db"
 )
 
 // listCmd represents the list command
@@ -11,7 +13,22 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Lists all of your tasks.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		tasks, err := db.AllTasks()
+
+		if err != nil {
+			fmt.Println("Something went wrong:", err.Error())
+			os.Exit(1)
+		}
+
+		if len(tasks) == 0 {
+			fmt.Println("You have no tasks available")
+			return
+		}
+
+		fmt.Println("You have the following tasks:")
+		for i, task := range tasks {
+			fmt.Printf("%d. %s\n", i+1, task.Value)
+		}
 	},
 }
 
