e1d6a6a5018dc70f66074489b50943b9d1957e66
Divine <officialkb17@gmail.com>
2024-09-09 17:52:52 +0100

    Create db bucket

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/vineyy17/cli-task-manager
 go 1.22.4
 
 require (
+	github.com/boltdb/bolt v1.3.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/vineyy17/cli-task-manager/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/vineyy17/cli-task-manager/cmd"
+	"github.com/vineyy17/cli-task-manager/db"
+)
 
 func main() {
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
 	cmd.Execute()
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
