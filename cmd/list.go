642e53450a68fd43fa1385e2a4b879a805ff6c3a
Divine <officialkb17@gmail.com>
2024-09-11 08:04:19 +0100

    Add ability to list relevant tasks or all tasks by adding the "-a" or "--all" flag

--- cmd/list.go
+++ cmd/list.go
@@ -3,18 +3,19 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
 	"github.com/vineyy17/cli-task-manager/db"
 )
 
-// listCmd represents the list command
+var listAll bool
+
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Lists all of your tasks.",
 	Run: func(cmd *cobra.Command, args []string) {
 		tasks, err := db.AllTasks()
-
 		if err != nil {
 			fmt.Println("Something went wrong:", err)
 			os.Exit(1)
@@ -25,13 +26,36 @@ var listCmd = &cobra.Command{
 			return
 		}
 
-		fmt.Println("You have the following tasks:")
-		for i, task := range tasks {
-			fmt.Printf("%d. %s\n", i+1, task.Value)
+		// Prepare to format output
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
+
+		if listAll {
+			fmt.Fprintln(w, "S/N\tTask\tCreated\tDone")
+		} else {
+			fmt.Fprintln(w, "S/N\tTask\tCreated")
 		}
+
+		// Filter and reorder tasks
+		var displayedTasks []db.Task
+		for _, task := range tasks {
+			if listAll || !task.Done {
+				displayedTasks = append(displayedTasks, task)
+			}
+		}
+
+		// Print tasks with renumbered S/N
+		for i, task := range displayedTasks {
+			if listAll {
+				fmt.Fprintf(w, "%d\t%s\t%s\t%v\n", i+1, task.Value, db.FormatCreatedTime(task.Created), task.Done)
+			} else {
+				fmt.Fprintf(w, "%d\t%s\t%s\n", i+1, task.Value, db.FormatCreatedTime(task.Created))
+			}
+		}
+		w.Flush()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+	listCmd.Flags().BoolVarP(&listAll, "all", "a", false, "Show all tasks, including completed ones")
 }
